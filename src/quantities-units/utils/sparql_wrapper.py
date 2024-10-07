46c2290f2ec85c5de11990ae0b596d5ef2e673cb
Andreas Raeder <andreas.raeder@isc.fraunhofer.de>
2024-10-07 07:36:16 +0000

    update to current state of the app

--- src/quantities-units/utils/sparql_wrapper.py
+++ src/quantities-units/utils/sparql_wrapper.py
@@ -2,7 +2,8 @@
 
 import os
 import pathlib
-from SPARQLWrapper import SPARQLWrapper, GET, POST, JSON
+import json
+from SPARQLWrapper import SPARQLWrapper, GET, POST, JSON  # noqa
 
 
 class Sparql:
@@ -10,10 +11,11 @@ class Sparql:
 
     def __init__(
         self,
-        method=POST,
+        method=GET,
         return_format=JSON,
         endpoint="https://www.qudt.org/fuseki/qudt/sparql",
-        filepath="",
+        src_filepath="",
+        tgt_filepath="",
         debug=False,
         query="",
     ):
@@ -21,11 +23,12 @@ class Sparql:
         self.sparql_method = method
         self.sparql_return_format = return_format
         self.sparql_endpoint = endpoint
-        self.filepath = filepath
+        self.src_filepath = src_filepath
+        self.tgt_filepath = tgt_filepath
         self.debug = debug
         # Read the SPARQL query from a file if a file path is provided
-        if self.filepath:
-            self.sparql_query = self.readFile()
+        if self.src_filepath:
+            self.sparql_query = self.readSparqlFile()
         else:
             print("Using the provided SPARQL Query...")
             self.sparql_query = query
@@ -44,7 +47,7 @@ class Sparql:
             print("\n")
         return result
 
-    def readFile(self):
+    def readSparqlFile(self):
         """
         Reads the content of a .sparql file in an OS-independent manner.
 
@@ -53,13 +56,13 @@ class Sparql:
         """
         try:
             # Check if path is a relative or absolute path
-            if not os.path.isabs(self.filepath):
+            if not os.path.isabs(self.src_filepath):
                 basedir = pathlib.Path(__file__).parent
-                filepath = os.path.join(basedir, self.filepath)
+                src_filepath = os.path.join(basedir, self.src_filepath)
 
             # Ensure the path is OS independent
-            print(f"Reading SPARQL Query from {filepath}")
-            normalizedPath = os.path.normpath(filepath)
+            print(f"Reading SPARQL Query from {src_filepath}")
+            normalizedPath = os.path.normpath(src_filepath)
 
             # Check if the file has a .sparql suffix
             if not normalizedPath.endswith(".sparql"):
@@ -76,6 +79,39 @@ class Sparql:
             print(f"An error occurred: {e}")
             return None
 
+    def writeJsonFile(self, data={}):
+        """
+        Writes the given data to a .json file in an OS-independent manner.
+
+        Parameters:
+        data (dict): The data to write to the file.
+        """
+        try:
+            # Check if path is a relative or absolute path
+            if not os.path.isabs(self.tgt_filepath):
+                basedir = pathlib.Path(__file__).parent
+                tgt_filepath = os.path.join(basedir, self.tgt_filepath)
+            else:
+                tgt_filepath = self.tgt_filepath
+
+            # Ensure the path is OS independent
+            print(f"Writing JSON data to {tgt_filepath}")
+            normalizedPath = os.path.normpath(tgt_filepath)
+
+            # Check if the file has a .json suffix
+            if not normalizedPath.endswith(".json"):
+                raise ValueError("File does not have a .json suffix")
+
+            # Write the file
+            with open(normalizedPath, "w") as file:
+                json.dump(data, file, indent=4)
+
+            if self.debug:
+                print(f"Data successfully written to {normalizedPath}")
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
 
 if __name__ == "__main__":
     from pprint import pprint
@@ -83,7 +119,7 @@ if __name__ == "__main__":
     # Qudt instance
     sparql_qudt = Sparql(
         endpoint="https://www.qudt.org/fuseki/qudt/sparql",
-        filepath="../ontology/qudt/sparql/quantitykind.sparql",
+        src_filepath="../ontology/qudt/sparql/quantitykind.sparql",
         debug=False,
     )
     qudt_qk = sparql_qudt.execQuery()
@@ -92,7 +128,7 @@ if __name__ == "__main__":
     # Wikidata instance
     sparql_wikidata = Sparql(
         endpoint="https://query.wikidata.org/sparql",
-        filepath="../ontology/wikidata/sparql/quantitykind.sparql",
+        src_filepath="../ontology/wikidata/sparql/quantitykind.sparql",
         debug=False,
     )
     wikidata_qk = sparql_wikidata.execQuery()
@@ -103,8 +139,9 @@ if __name__ == "__main__":
         method=POST,
         # endpoint="http://www.ontology-of-units-of-measure.org/resource/om-2/",
         endpoint="https://fuseki.sysmon.digital.isc.fraunhofer.de/om2/sparql",
-        filepath="../ontology/om2/sparql/dimension.sparql",
+        src_filepath="../ontology/om2/sparql/dimension.sparql",
         debug=True,
     )
     om2_qk = sparql_om2.execQuery()
+
     pprint(om2_qk)
