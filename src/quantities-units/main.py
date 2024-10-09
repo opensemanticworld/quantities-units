77ee5cbb9d1e9973ca5fdfe4d99a34284267ebcb
Andreas Raeder <andreas.raeder@isc.fraunhofer.de>
2024-10-09 15:03:24 +0200

    add: feature unit prefix osl import works, tbd in .py

--- src/quantities-units/main.py
+++ src/quantities-units/main.py
@@ -6,11 +6,23 @@ from utils.sparql_query import execQuery
 
 def main():
     """Main entrypoint for the quantities-units app."""
-    print("Reading SPARQL Query File...")
-    query = readSparqlFile("queries/quantity_units.sparql")
-    if query:
-        print("Executing SPARQL Query...")
-        execQuery(query, debug=True)
+    # 1. Unit Prefix
+    # --------------
+    # https://wiki-dev.open-semantic-lab.org/wiki/Category:OSW99e0f46a40ca4129a420b4bb89c4cc45 # noqa
+    # TODO: implement utility from 1_unit_prefix.ipynb and add function call or prefect workflow here
+
+    # 2. Prefixed Units
+    # -----------------
+
+    # 3. Fundamental Units
+    # --------------------
+
+    # 4. Quantities
+    # -------------
+    # Broader -> QuantityKind + Characteristic
+    # Narrower -> Characteristic
+
+    # Step 3: Define and upload characteristics
 
 
 if __name__ == "__main__":
