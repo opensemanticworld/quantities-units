4dab83612de92a4f05d0dcde0d45f1943c4ab27f
Matthias Albert Popp <matthias.albert.popp@isc.fraunhofer.de>
2024-10-10 08:46:34 +0200

    gut unit prefix

--- src/quantities-units/extract_units.py
+++ src/quantities-units/extract_units.py
@@ -29,6 +29,13 @@ def get_unit_prefix(unit_str):
         if prefix in unit_str.lower():
             return prefix
     return None
+
+def get_main_string(unit_str):
+    for prefix in prefix_name_list:
+        capitalized_prefix = prefix.capitalize()
+        if capitalized_prefix in unit_str:
+            return unit_str.replace(capitalized_prefix, "")
+    return unit_str
 def get_non_prefixed_units_from_applicable_units(applicable_units_str):
     non_prefixed_units = []
     prefixed_units = []
@@ -44,6 +51,13 @@ def get_non_prefixed_units_from_applicable_units(applicable_units_str):
             prefixed_units.append(unit_str)
     return non_prefixed_units, prefixed_units
 
+def build_unit_dict(prefixed_unit_strings, non_prefixed_unit_strings):
+    unit_dict = {}
+    for unit_str in non_prefixed_unit_strings:
+        unit_dict[unit_str] = {"main_symbol": "unit_str"}
+
+    for unit_str in prefixed_unit_strings:
+        pass
 
 all_prefixed_units = []
 all_non_prefixed_units = []
@@ -57,3 +71,9 @@ all_non_prefixed_units = list(set(all_non_prefixed_units))
 
 print(len(all_prefixed_units))
 print(len(all_non_prefixed_units))
+
+if __name__ == "__main__":
+    for unit_str in all_prefixed_units:
+        base_string = get_main_string(unit_str)
+        print(base_string)
+        print(base_string in all_non_prefixed_units)
